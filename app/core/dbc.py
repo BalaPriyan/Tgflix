e4a42223e305f617b1aa90038146eb3f0372fb9e
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2024-01-02 18:58:21 +0530

    Update dbc.py

--- app/core/dbc.py
+++ app/core/dbc.py
@@ -3,11 +3,11 @@ from databases import Database
 from .config import CONFIG
 
 from sqlalchemy import MetaData
-from databases import Database, DatabaseError  # Import the DatabaseError
+from databases import Database# Import the DatabaseError
 
 try:
     db = Database(CONFIG.DATABASE_URL)
-except DatabaseError as e:
+except Database as e:
     raise ValueError(f'Error connecting to the database: {e}')  # Customized error message with details
 except Exception as e:
     raise ValueError(f'Unexpected error: {e}')  # Catching any unexpected exceptions
