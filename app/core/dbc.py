ec6e9935afce615fd1b491e1db9952477d86cbd5
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2024-01-02 18:50:57 +0530

    Update dbc.py

--- app/core/dbc.py
+++ app/core/dbc.py
@@ -3,8 +3,10 @@ from databases import Database
 from .config import CONFIG
 
 try:
-    db = Database( CONFIG.DATABASE_URL)
-except:
-    raise('Invalid or missing database url')
+    db = Database(CONFIG.DATABASE_URL)
+except DatabaseError as e:
+    raise ValueError(f'Error connecting to the database: {e}')  # Customized error message with details
+except Exception as e:
+    raise ValueError(f'Unexpected error: {e}')  # Catching any unexpected exceptions
 metadata = MetaData()
 
